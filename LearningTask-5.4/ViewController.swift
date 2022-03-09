b2c36b4e8923271c584a22330f0788ebcc181648
Rafael Rollo <rafaelrollo92@gmail.com>
2022-03-09 13:34:44 +0000

    Initial commit

---
+++ LearningTask-5.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningTask-5.4
+//
+//  Created by rafael.rollo on 09/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# emi-learning-task-5.4
+Explorando o Mundo iOS - Learning Task 5.4
